6fe4a5463fbfa48a2c164accb7d880b9ebbadeb7
Zuura <zuura@MacBook-Air-Zuura.local>
2023-06-27 19:28:19 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,72 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+ /*       System.out.println(method(5,7,6,9));
+     }
+        static  int method(int...var){
+        int sum=0;
+        for (int i= 0; i < var.length ;i++) {
+          sum+=var[i] ;
+        }
+         return  sum;            */
+
+
+  /*      String[] students = {"Aleriza","Baikal","Akmaral","Ruslan","Ruslan","Aidai","Aizirek","Nurtegin",
+                "Bermet","Jamal","Zhanylai","Zuura","Bekbolsun","Nazgul","Askar","Rysbek","Nazgul","Begimai","Aizat",
+                "Aiperi","Nurjigit","Kumar","Samagan","Bibolot","Sezim","Chyngyz","Ruslan"};
+
+           for(String name : students){
+            if(name.equals("Ruslan")){
+                System.out.println("the best boy "+name);
+            }
+            else {
+                System.out.println(name + " the best student");     */
+
+
+
+  /*      System.out.println(method(5,78,54,9,23,45,8,45));
+    }
+           static int method ( int ...var){
+            int sum = 0;
+            int count = 0;
+
+            for (int i = 0; i < var.length; i++) {
+                sum += var[i];
+                count++;
+            }
+            System.out.println(var.length);
+            System.out.println(count);
+            return sum;                      */
+
+
+/*        System.out.println(method());
+
+    }
+    static int method() {
+        int summa = 0;
+        for (int i = 0; i <= 300; i++) {
+            if (i % 3 == 0 && i % 4 == 0 && i % 5 == 0) {
+                System.out.println(i);
+                summa += 1;
+            }
+        }
+        return summa;         */
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
